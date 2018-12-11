e0f17e901c3f3edd50152d958535b4a0e798d6c6
Esther <esther.spiteri.18@um.edu.mt>
2018-12-11 13:35:36 +0100

    Added files

---
+++ cmake-build-debug/CMakeFiles/assignment3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment3.dir/main.c.obj"
+  "assignment3.pdb"
+  "assignment3.exe"
+  "assignment3.exe.manifest"
+  "libassignment3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
