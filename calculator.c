577c4edc53fe81844413e2ff3d9392b1a778ec7e
Esther <esther.spiteri.18@um.edu.mt>
2018-12-12 12:07:50 +0100

    Started calculator program

---
+++ calculator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    char operator;
+    float firstNumber,secondNumber;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter two operands: ");
+    scanf("%f %f",&firstNumber, &secondNumber);
+}
--- cmake-build-debug/CMakeFiles/calculator.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/calculator.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/Owner/Desktop/uni/collaborative project/assignment3/Code-Busters/main.c" "C:/Users/Owner/Desktop/uni/collaborative project/assignment3/Code-Busters/cmake-build-debug/CMakeFiles/assignment3.dir/main.c.obj"
+  "C:/Users/Owner/Desktop/uni/collaborative project/assignment3/Code-Busters/calculator.c" "C:/Users/Owner/Desktop/uni/collaborative project/assignment3/Code-Busters/cmake-build-debug/CMakeFiles/calculator.dir/calculator.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
